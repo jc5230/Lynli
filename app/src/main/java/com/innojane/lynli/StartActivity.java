8a16cea2125980b581c1237a0bf1b95fec9e0628
jc5230 <jc5230@tc.columbia.edu>
2021-07-04 12:57:46 -0400

    Start activity

---
+++ app/src/main/java/com/innojane/lynli/StartActivity.java
@@ -0,0 +1,15 @@
+package com.innojane.lynli;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
